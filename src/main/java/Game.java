0f51bda7f605bdc3a967107e57880def9e76c480
KZwolski <kkzwolski@gmail.com>
2022-07-26 21:38:55 +0200

    Input validation

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 public class Game {
     public static void main(String[] args) {
         Movement movement = new Movement();
-        System.out.println("Enter size: ");
-        Scanner scanner = new Scanner(System.in);
-        int size = scanner.nextInt();
-        Board board = new Board(size);
+        int boardSize = movement.askForBoardSize();
+        Board board = new Board(boardSize);
         board.printBoard();
-
-        board.movementPhase(movement.askForInput());
+        int [] coordinates = movement.playersMove(board,1);
+        board.movementPhase(coordinates);
         board.printBoard();
 
 
--- src/main/java/Movement.java
+++ src/main/java/Movement.java
@@ -1,12 +1,56 @@
 import java.util.Scanner;
-
+import java.util.Locale;
 public class Movement {
-    public int[] askForInput () {
+    public String askForInput () {
+        Scanner cords = new Scanner(System.in);
+        System.out.println("Please enter coordinates: ");
+        String move = cords.nextLine().toUpperCase(Locale.ROOT);
+        return move;
+    }
+    public boolean checkForValidInput(String input, int size) {
+        try{
+            int column = convertInputIntoColumn(input);
+            int row = convertInputIntoRow(input);
+            return ((column <= size)&&(row <= size));
+        }
+        catch (Exception e){
+            return false;
+        }
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        int inputX = scanner.nextInt();
-        int inputY = scanner.nextInt();
-        return new int[]{inputX, inputY};
+    public int convertInputIntoColumn(String input){
+        int row = input.charAt(0) - 'A';
+        return row;
+    }
+    public int convertInputIntoRow(String input){
+        int column = Integer.parseInt(input.substring(1))- 1;
+        return column;
+    }
+    public int[] playersMove(Board board, int player){
+        while(true){
+            String input = askForInput();
+            if(checkForValidInput(input, board.board.length)){
+               int column = convertInputIntoColumn(input);
+               int row = convertInputIntoRow(input);
+               if(board.board[row][column].isActive() && board.board[row][column].getColor() == player){
+                   int coordinates[] = new int[2];
+                   coordinates[0]=row;
+                   coordinates[1]=column;
+                   return coordinates;
+               }
+           }
+        }
+    }
+    public int askForBoardSize (){
+        while(true) {
+            System.out.println("Please enter board size (must be an even number between 10-20): ");
+            Scanner scanner = new Scanner(System.in);
+            int size = scanner.nextInt();
+            if ((size >= 10 && size <= 20) && size % 2 == 0) {
+                return size;
+            }
+        }
+
     }
 
 
