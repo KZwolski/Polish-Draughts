8014a12acb4bbc0e73ab42b2d811c1102f75422a
mat-kossowski <mat.kossowski.pl@gmail.com>
2022-07-26 10:32:09 +0200

    printing a beautiful board//refactoring

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -29,13 +29,18 @@ public class Board {
     public void printBoard() {
 
         for (int i = 1; i <= this.board.length; i++) {
-            System.out.print(i + " ");
+            if(i<10){
+                System.out.print(i + "  ");
+            }else {
+                System.out.print(i + " ");
+            }
+
             for (int j = 0; j < this.board[i - 1].length; j++) {
                 System.out.print(this.board[i - 1][j]);
             }
             System.out.println();
         }
-        System.out.print("  ");
+        System.out.print("   ");
         for (int i = 1; i <= board.length; i++) {
             System.out.print(" " + (char) (64 + i) + " ");
         }
