ba89b89af261d77c617df55120a71817076677a3
mat-kossowski <mat.kossowski.pl@gmail.com>
2022-07-26 08:56:36 +0200

    printing a beautiful board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -6,18 +6,38 @@ public class Board {
         board = new String[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                board[i][j] = "0";
+                if (i % 2 == 0) {
+                    if (j % 2 == 0) {
+                        board[i][j] = "[ ]";
+                    } else {
+                        board[i][j] = "[" + String.valueOf((char) (35))+ "]";
+                    }
+                } else {
+                    if (j % 2 == 0) {
+
+                        board[i][j] = "[" + String.valueOf((char) (35))+ "]";
+                    } else {
+                        board[i][j] = "[ ]";
+                    }
+
+                }
             }
         }
 
     }
 
     public void printBoard() {
-        for (int i = 0; i < this.board.length; i++) {
-            for (int j = 0; j < this.board[i].length; j++) {
-                System.out.print(this.board[i][j]+ " ");
+
+        for (int i = 1; i <= this.board.length; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < this.board[i - 1].length; j++) {
+                System.out.print(this.board[i - 1][j]);
             }
             System.out.println();
+        }
+        System.out.print("  ");
+        for (int i = 1; i <= board.length; i++) {
+            System.out.print(" " + (char) (64 + i) + " ");
         }
 
     }
