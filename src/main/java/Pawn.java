18b8b58cf469a118809ad7a2b2c5b5a549ddc2d8
Szymon Tracz <szymontracz1@gmail.com>
2022-07-26 10:46:09 +0200

    gotowe

--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -1,2 +1,45 @@
 public class Pawn {
+    private String color;
+    private int X;
+    private int Y;
+    private boolean isCrowned;
+
+    public Pawn(String color, int x, int y, boolean isCrowned) {
+        this.color = color;
+        X = x;
+        Y = y;
+        this.isCrowned = isCrowned;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    public boolean isCrowned() {
+        return isCrowned;
+    }
+
+    public void setCrowned(boolean crowned) {
+        isCrowned = crowned;
+    }
 }
